4e2b32a19d97c80a9b8bb609e18b614387c51514
松永菜月 <dicaiyue856@gmail.com>
2026-06-30 18:01:40 +0900

    お問い合わせ確認フォームのUIの実装

--- src/resources/views/confirm.blade.php
+++ src/resources/views/confirm.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('css')
-<link rel="stylesheet" href="{{ asset('css/confirm.css') }}">
+<link rel="stylesheet" href="{{ asset('css/index.css') }}">
 @endsection
 
 @section('title','お問い合わせ確認フォーム')
@@ -12,144 +12,92 @@
         <h2>Confirm</h2>
     </div>
     <!-- フォームのアクション設定TODO -->
-    <form class="form" action="/store" method="post" novalidate>
+    <form class="" action="/store" method="post" novalidate>
         @csrf
-        {{--お名前 確認欄 --}}
-        <div class="form__confirm">
-            <div class="form__confirm-title">
-                <span class="form__label--confirm">お名前</span>
-            </div>
-            <div class="form__group-confirm">
-                <div class="form__confirm--text">
-                    <div class="form__name--confirm">
-                        {{--
-                            <input class="first_name" type="text" name="first_name" value="{{ $contact['first_name'] }}" readonly />
-                        <input class="last_name" type="text" name="last_name" value="{{ $contact['last_name'] }}" readonly />
-                        --}}
+        <table class="confirm__table">
+            {{--お名前 確認欄 --}}
+            <tr class="form__confirm">
+                <th class="form__label--confirm">お名前</th>
+                <td class="form__group-confirm">
+                    <p>{{ $contact['first_name'] }}　{{ $contact['last_name'] }}</p>
+                    <input type="hidden" name="first_name" value="{{ $contact['first_name'] }}">
+                    <input type="hidden" name="last_name" value="{{ $contact['last_name'] }}">
+                </td>
+            </tr>
 
-                        <p>{{ $contact['first_name'] }}　{{ $contact['last_name'] }}</p>
-                        <input type="hidden" name="first_name" value="{{ $contact['first_name'] }}">
-                        <input type="hidden" name="last_name" value="{{ $contact['last_name'] }}">
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        {{-- 性別 確認欄 --}}
-        <div class="form__confirm">
-            <div class="form__label--confirm">
-                <span class="form__confirm--item">性別</span>
-                <div class="form__group-confirm">
-                    @if ($contact['gender'] == 'male')
+            {{-- 性別 確認欄 --}}
+            <tr class="form__confirm">
+                <th class="form__label--confirm">性別</th>
+                <td class="form__group-confirm" style="height: 50px;">
+                    @if ($contact['gender'] == '0')
                     男性
-                    @elseif ($contact['gender'] == 'female')
+                    @elseif ($contact['gender'] == '1')
                     女性
                     @else
                     その他
                     @endif
                     <input type="hidden" name="gender" value="{{ $contact['gender'] }}">
-                </div>
-            </div>
-        </div>
+                </td>
+            </tr>
 
-        {{-- メールアドレス 確認欄 --}}
-        <div class="form__confirm">
-            <div class="form__confirm-title">
-                <span class="form__label--confirm">メールアドレス</span>
-            </div>
-            <div class="form__group-confirm">
-                {{--
-                    <div class="form__confirm--text">
-                        <input type="email" name="email" value="{{ $contact['email'] }}" readonly />
-            </div>
-            --}}
+            {{-- メールアドレス 確認欄 --}}
+            <tr class="form__confirm">
+                <th class="form__label--confirm">メールアドレス</th>
+                <td class="form__group-confirm">
+                    <p>{{ $contact['email'] }}</p>
+                    <input type="hidden" name="email" value="{{ $contact['email'] }}">
+                </td>
+            </tr>
 
-            <p>{{ $contact['email'] }}</p>
-            <input type="hidden" name="email" value="{{ $contact['email'] }}">
-        </div>
+            {{-- 電話番号 確認欄 --}}
+            <tr class="form__confirm">
+                <th class="form__label--confirm">電話番号</th>
+                <td class="form__group-confirm">
+                    <div class="form__confirm--tel">
+                        <p>{{ $contact['tel1'] }} <span>-</span> {{ $contact['tel2'] }} <span>-</span> {{ $contact['tel3'] }}</p>
+                        <input type="hidden" name="tel1" value="{{ $contact['tel1'] }}">
+                        <input type="hidden" name="tel2" value="{{ $contact['tel2'] }}">
+                        <input type="hidden" name="tel3" value="{{ $contact['tel3'] }}">
+                    </div>
+                </td>
+            </tr>
 
-        {{-- 電話番号 確認欄 --}}
-        <div class="form__confirm">
-            <div class="form__confirm-title">
-                <span class="form__label--confirm">電話番号</span>
-            </div>
-            <div class="form__group-confirm">
-                <div class="form__confirm--tel">
-                    {{--
-                                <input type="tel" name="tel1" maxlength="3" placeholder="080" readonly />
-                                <span>-</span>
-                                <input type="tel" name="tel2" maxlength="4" placeholder="1234" readonly />
-                                <span>-</span>
-                                <input type="tel" name="tel3" maxlength="4" placeholder="5678" readonly />
-                            --}}
-
-                    <p>{{ $contact['tel1'] }} <span>-</span> {{ $contact['tel2'] }} <span>-</span> {{ $contact['tel3'] }}</p>
-                    <input type="hidden" name="tel1" value="{{ $contact['tel1'] }}">
-                    <input type="hidden" name="tel2" value="{{ $contact['tel2'] }}">
-                    <input type="hidden" name="tel3" value="{{ $contact['tel3'] }}">
-                </div>
-            </div>
-        </div>
-
-        {{-- 住所 確認欄 --}}
-        <div class="form__confirm">
-            <div class="form__confirm-title">
-                <span class="form__label--confirm">住所</span>
-            </div>
-            <div class="form__group-confirm">
-                <div class="form__confirm--text">
-                    {{--
-                                <input type="address" name="address" value="{{ $contact['address'] }}" readonly />
-                    --}}
-
+            {{-- 住所 確認欄 --}}
+            <tr class="form__confirm">
+                <th class="form__label--confirm">住所</th>
+                <td class="form__group-confirm">
                     <p>{{ $contact['address'] }}</p>
                     <input type="hidden" name="address" value="{{ $contact['address'] }}">
-                </div>
-            </div>
-        </div>
+                </td>
+            </tr>
 
-        {{-- 建物名 確認欄 --}}
-        <div class="form__confirm">
-            <div class="form__confirm-title">
-                <span class="form__label--confirm">建物名</span>
-            </div>
-            <div class="form__group-confirm">
-                <div class="form__confirm--text">
-                    {{--
-                                <input type="building" name="building" value="{{ $contact['building'] }}" readonly />
-                    --}}
-
+            {{-- 建物名 確認欄 --}}
+            <tr class="form__confirm">
+                <th class="form__label--confirm">建物名</th>
+                <td class="form__group-confirm">
                     <p>{{ $contact['building'] }}</p>
                     <input type="hidden" name="building" value="{{ $contact['building'] }}">
-                </div>
-            </div>
-        </div>
+                </td>
+            </tr>
 
-        {{-- お問い合わせの種類 確認欄 --}}
-        <div class="form__confirm">
-            <div class="form__confirm-title">
-                <span class="form__label--confirm">お問い合わせの種類</span>
-            </div>
-            <div class="form__group-confirm">
-                <div class="form__confirm--text">
+            {{-- お問い合わせの種類 確認欄 --}}
+            <tr class="form__confirm">
+                <th class="form__label--confirm">お問い合わせの種類</th>
+                <td class="form__group-confirm">
                     <p>{{ $category['content'] }}</p>
                     <input type="hidden" name="category_id" value="{{ $contact['category_id'] }}">
-                </div>
-            </div>
-        </div>
+                </td>
+            </tr>
 
-        {{-- お問い合わせ 確認欄 --}}
-        <div class="form__confirm">
-            <div class="form__confirm-title">
-                <span class="form__label--confirm">お問い合わせ内容</span>
-            </div>
-            <div class="form__group-confirm">
-                <div class="form__confirm--text">
+            {{-- お問い合わせ 確認欄 --}}
+            <tr class="form__confirm">
+                <th class="form__label--confirm">お問い合わせ内容</th>
+                <td class="form__group-confirm">
                     <p>{{ $contact['content'] }}</p>
                     <input type="hidden" name="content" value="{{ $contact['content'] }}">
-                </div>
-            </div>
-        </div>
+                </td>
+            </tr>
+        </table>
 
         {{-- フォームボタン --}}
         <div class="form__button">
