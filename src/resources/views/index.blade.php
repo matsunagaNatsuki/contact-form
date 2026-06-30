9110942667ed804af56fead22209931627f5968f
松永菜月 <dicaiyue856@gmail.com>
2026-06-30 15:11:43 +0900

    お問い合わせフォームのCSSの修正

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -11,13 +11,13 @@
     <div class="contact-form__heading">
         <h2>Contact</h2>
     </div>
-    <form class="form" action="/contacts" method="post" novalidate>
+    <form class="" action="/contacts" method="post" novalidate>
         @csrf
         {{--お名前 入力欄 --}}
-        <div class="form__group">
+        <div class="form">
             <div class="form__group-title">
-                <span class="form__label--item">お名前</span>
-                <span class="form__label--required">※</span>
+                <label class="form__label--item">お名前</label>
+                <label class="form__label--required">※</label>
             </div>
             <div class="form__group-content">
                 <div class="form__input--text">
@@ -37,13 +37,11 @@
                     </div>
                 </div>
             </div>
-        </div>
 
-        {{-- 性別 ラジオボタン --}}
-        <div class="form__group">
+            {{-- 性別 ラジオボタン --}}
             <div class="form__group-title">
-                <span class="form__label--item">性別</span>
-                <span class="form__label--required">※</span>
+                <label class="form__label--item">性別</label>
+                <label class="form__label--required">※</label>
             </div>
             <div class="form__group-content">
                 <div class="form__input--radio">
@@ -54,12 +52,12 @@
                     </label>
                     <label>
                         <input type="radio" name="gender" value="1" {{ old('gender') == '1' ? 'checked' : '' }}>
-                        
+
                         女性
                     </label>
                     <label>
                         <input type="radio" name="gender" value="2" {{ old('gender') == '2' ? 'checked' : '' }}>
-                        
+
                         その他
                     </label>
                 </div>
@@ -69,13 +67,11 @@
                     @enderror
                 </div>
             </div>
-        </div>
 
-        {{-- メールアドレス 入力欄 --}}
-        <div class="form__group">
+            {{-- メールアドレス 入力欄 --}}
             <div class="form__group-title">
-                <span class="form__label--item">メールアドレス</span>
-                <span class="form__label--required">※</span>
+                <label class="form__label--item">メールアドレス</label>
+                <label class="form__label--required">※</span>
             </div>
             <div class="form__group-content">
                 <div class="form__input--text">
@@ -87,13 +83,11 @@
                     @enderror
                 </div>
             </div>
-        </div>
 
-        {{-- 電話番号 入力欄 --}}
-        <div class="form__group">
+            {{-- 電話番号 入力欄 --}}
             <div class="form__group-title">
-                <span class="form__label--item">電話番号</span>
-                <span class="form__label--required">※</span>
+                <label class="form__label--item">電話番号</label>
+                <label class="form__label--required">※</label>
             </div>
             <div class="form__group-content">
                 <div class="form__input--tel">
@@ -110,13 +104,11 @@
                     @endif
                 </div>
             </div>
-        </div>
 
-        {{-- 住所 入力欄 --}}
-        <div class="form__group">
+            {{-- 住所 入力欄 --}}
             <div class="form__group-title">
-                <span class="form__label--item">住所</span>
-                <span class="form__label--required">※</span>
+                <label class="form__label--item">住所</label>
+                <label class="form__label--required">※</label>
             </div>
             <div class="form__group-content">
                 <div class="form__input--text">
@@ -128,27 +120,23 @@
                     @enderror
                 </div>
             </div>
-        </div>
 
-        {{-- 建物名 入力欄 --}}
-        <div class="form__group">
+            {{-- 建物名 入力欄 --}}
             <div class="form__group-title">
-                <span class="form__label--item">建物名</span>
+                <label class="form__label--item">建物名</label>
             </div>
             <div class="form__group-content">
                 <div class="form__input--text">
                     <input type="building" name="building" placeholder="例: 千駄ヶ谷マンション101" value="{{ old('building') }}" />
                 </div>
             </div>
-        </div>
 
-        {{-- お問い合わせの種類 プルタブ選択 --}}
-        <div class="form__group">
+            {{-- お問い合わせの種類 プルタブ選択 --}}
             <div class="form__group-title">
-                <span class="form__label--item">お問い合わせの種類</span>
-                <span class="form__label--required">※</span>
+                <label class="form__label--item">お問い合わせの種類</label>
+                <label class="form__label--required">※</label>
             </div>
-            <div class="form__group-content">
+            <div class="form__group-content" style="margin-top:6px;">
                 <div class="form__option--select">
                     <select name="category_id">
                         <option value="">選択してください</option>
@@ -165,12 +153,10 @@
                     @enderror
                 </div>
             </div>
-        </div>
 
-        {{-- お問い合わせ 入力欄 --}}
-        <div class="form__group">
+            {{-- お問い合わせ 入力欄 --}}
             <div class="form__group-title">
-                <span class="form__label--item">お問い合わせ内容</span>
+                <label class="form__label--item">お問い合わせ内容</label>
             </div>
             <div class="form__group-content">
                 <div class="form__input--textarea">
@@ -183,11 +169,10 @@
                 </div>
             </div>
         </div>
-
         {{-- お問い合わせ 送信ボタン --}}
         <div class="form__button">
             <button class="form__button-submit" type="submit">確認画面</button>
         </div>
-    </form>
+</form>
 </div>
 @endsection
